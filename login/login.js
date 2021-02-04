4bb24656056bb2791a6cb82319a4bf977a594311
Pedro Henrique Viana <pedro.henrique@categoriadebase.org>
2021-02-04 09:42:05 -0300

    Finalização da tela de Login
    Ajustes feitos na pagina

--- login/login.js
+++ login/login.js
@@ -1,4 +1,5 @@
 function entrar() {
+    debugger;
     var celular = document.getElementById("celular").value;
     var senha = document.getElementById("senha").value;
 
@@ -6,15 +7,21 @@ function entrar() {
     //a senha do usuario é 12345
 
     if(celular === "+5519989876305" && senha === "12345"){
-        var erros = document.getElementsByClassName("form--error");
-        for(var contador = 0; erros.length; contador++){
-          erros[contador].style.display="none";
-        }
-        alert("Parabens ta Logado");
+        // var erros = document.getElementsByClassName("form--error");
+        // for(var contador = 0; erros.length; contador++){
+        //   erros[contador].style.display="none";
+        // }
+        alert("Parabéns você tá Logado"); //não funciona fora do for
     }else{
       var erros = document.getElementsByClassName("form--error");
       for(var contador = 0; erros.length; contador++){
         erros[contador].style.display="block";
       }
     }
+}
+
+function cadastro() { 
+  window.setTimeout(() => {
+    window.location = "https://www.google.com/webhp?hl=pt-BR&sa=X&ved=0ahUKEwj7z7zmk9DuAhV8H7kGHS9uCPsQPAgI";
+ }, 100);
 }
