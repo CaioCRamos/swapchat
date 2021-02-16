ec388b28eb95b80f14712982d3d1aa593f2f37e8
Milena Vitoria Martins Alves <78030806+MilenaAlves22@users.noreply.github.com>
2021-02-16 10:47:46 -0300

    backend da tela de cadastro-usuario

--- cadastro-usuario/cadastro-usuario.js
+++ cadastro-usuario/cadastro-usuario.js
@@ -1,15 +1,40 @@
-function continuar() {
-    var nome = document.getElementById("nome_input").Value;
+ async function continuar() {
+    var nome = document.getElementById("nome_input").value;
     var celular = document.getElementById("celular_input").value;
-    var senha = documente.getElementById("senha_input").value;
+    var senha = document.getElementById("senha_input").value;
+    var pergunta= document.getElementById("caixa_pergunta").value;
     var resposta = document.getElementById("resposta_input").value;
 
+    debugger;
+    if (nome !== "" && celular !== "" && senha !== "" && pergunta !== "" && resposta !== "" ) {
+        var corpoJson = {
+            nome: nome,
+            celular: celular,
+            senha: senha,
+            perguntaSeguranca: pergunta,
+            respostaSeguranca: resposta
+        };
+    
+        var resposta = await fetch("https://swapchat-api.herokuapp.com/v1/users", {
+            method: "POST",
+            body: JSON.stringify(corpoJson),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        var respostaJson = await resposta.json();
 
-    if (nome === "" && celular === "" && senha === "" && resposta === "") {
-        alert("Cadastro efetuado com sucesso!");
-
+        if (resposta.status === 201) {
+            localStorage.setItem("id", respostaJson.id);
+            alert(respostaJson.mensagem);
+            window.location.href = "../cadastro-conta/cadastro-conta.html";
+        }
     } else {
-        var error = document.getElementsByClassName("form--error");
-        for (var contador = 0; contador < error.length; contador++);
+        var erros = document.getElementsByClassName("form--error");
+        for (var contador = 0; contador < erros.length; contador++){
+            erros[contador].style.display = "block";
+        }
     }
+
 }
