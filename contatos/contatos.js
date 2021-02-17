7a2d0ee549cd6ea35de6ce0254d21ea5c45bff1f
Milena Vitoria Martins Alves <78030806+MilenaAlves22@users.noreply.github.com>
2021-02-17 12:05:16 -0300

    ajustes

--- contatos/contatos.js
+++
@@ -1,5 +0,0 @@
-var ListaContatos = document.getElementById("lista_contatos");
-var modelo = document.getElementById("modeloContato");
-var novoContato = modelo.content.cloneNode(true);
-
-novoContato.querySelector(".icone_contato").src = "../imagens/icone-contato.png";
