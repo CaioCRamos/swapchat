6b626e025cb0419b9e02b538f2009ea763947342
Milena Vitoria Martins Alves <78030806+MilenaAlves22@users.noreply.github.com>
2021-02-22 10:51:20 -0300

    backend das telas de contatos e novo-contato finalizado
    -caminhos das telas concluído.
    -backend finalizado.

--- contatos/contatos.js
+++ contatos/contatos.js
@@ -1,10 +1,11 @@
 
-window.onload =  async function () {
+window.onload = async function () {
     await listarContatos();
 }
 
- async function listarContatos() {
-    var resposta = await fetch("https://swapchat-api.herokuapp.com/v1/contacts", {
+async function listarContatos() {
+    var idContaUsuario = localStorage.getItem("idContaSelecionada");
+    var resposta = await fetch("https://swapchat-api.herokuapp.com/v1/contacts/" + idContaUsuario, {
         method: "GET",
         headers: {
             'Content-Type': 'application/json',
@@ -20,18 +21,44 @@ window.onload =  async function () {
         var nome = contatos[contador].nome;
         var celular = contatos[contador].celular;
         var imagem = contatos[contador].imagem;
+        var idContato = contatos[contador].id;
 
         var novoContato = modelo.content.cloneNode(true);
         novoContato.querySelector(".icone_contato").src = imagem;
         novoContato.querySelector(".detalhe-nome").innerHTML = nome;
         novoContato.querySelector(".detalhe-telefone").innerHTML = celular;
+        novoContato.querySelector(".idContato").value = idContato;
+    
         listaContatos.appendChild(novoContato);
+    }
+}
+
+async function selecionarContato(li) {
+    debugger;
+
+    var idContato = li.querySelector(".idContato").value;
+    if (idContato === ""){
+        alert("Esse contato não possui conta no swapchat");
+        return;
     }
 
 
-
+    var idContaUsuario = localStorage.getItem("idContaSelecionada");
+    var corpoJson = {
+        idContaUsuario1: idContaUsuario,
+        idContaUsuario2: idContato
+    };
+    var resposta = await fetch("https://swapchat-api.herokuapp.com/v1/chats", {
+        method: "POST",
+        body: JSON.stringify(corpoJson),
+        headers: {
+            'Content-Type': 'application/json',
+            "x-access-token": localStorage.getItem("token")
+        }
+    });
+    if (resposta.status === 201) {
+        window.location.href = "../conversas/conversa.html";
+    }
 }
-
-
 
 
