3c3c5690f08a741e87ad3bab5d7b0ea315dff1a4
ARTHUR LUIZ DA SILVA BORGES <arthur_lb@hotmail.com>
2021-02-24 09:42:57 -0300

    atualização perfil

--- perfil/perfil.js
+++ perfil/perfil.js
@@ -1,3 +1,19 @@
 function salvar(){
     alert("obrigado por entrar no swapchat, logo sua nova conta estara logada agradecemos sua colaboração  ")
 }
+
+window.onload = function(){
+    var conta1= JSON.parse(localStorage.getItem("conta1"));
+    var conta2 = JSON.parse(localStorage.getItem("conta2"));
+
+    if(localStorage.getItem("idContaSelecionada") === conta1.id){
+        document.getElementById("foto").src = conta1.imagem;
+        document.getElementById("nome").value = conta1.nome;
+        document.getElementById("email").value = conta1.email;
+    } else{
+        document.getElementById("foto").src = conta2.imagem;
+        document.getElementById("nome").value = conta2.nome;
+        document.getElementById("email").value = conta2.email;
+    }
+
+}
